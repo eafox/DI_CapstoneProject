17d26186f1594568a18331df9485847e76c74737
Emma Fox <eafox@g.ucla.edu>
2018-07-26 13:11:30 -0400

    2nd map base

--- app.py
+++ app.py
@@ -29,11 +29,16 @@ app.vars={}
 @app.route('/',methods=['GET','POST'])
 def index():
     baseDF=getMap("Data/data_store/ESRI/London_Borough_Excluding_MHW.shp")
-    tempDF = makeDF(baseDF,"Data/00_FullDF.csv")
-    tempPlot1 = plot1(tempDF)
+    tempDF1 = makeDF1(baseDF,"Data/00_FullDF.csv")
+    tempPlot1 = plot1(tempDF1)
+
+    tempDF2 = makeDF2(baseDF,"Data/00_FullDF.csv")
+    tempPlot2 = plot2(tempDF2)
+
 	
-    script1, div1 = components(tempPlot1)		
-    return render_template('index.html',tempScript1=script1,tempDiv1=div1)
+    script1, div1 = components(tempPlot1)
+    script2, div2 = components(tempPlot2)		
+    return render_template('index.html',tempScript1=script1,tempDiv1=div1,tempScript2=script2,tempDiv2=div2)
 
 
 ################
@@ -124,12 +129,14 @@ def getMap(inFile):
     braw=gpd.read_file(inFile)        
     braw['x'] = braw.apply(getCoords, geom_col="geometry", coord_type="x", axis=1)
     braw['y'] = braw.apply(getCoords, geom_col="geometry", coord_type="y", axis=1)
+    for i in range(len(braw)):
+        braw.at[i,"x"],braw.at[i,"y"]=[val for val in braw.at[i,"x"] if str(val)!='nan'],[val for val in braw.at[i,"y"] if str(val)!='nan']
     return(braw)
 
 #####################
 # GET INCIDENT DATA #
 #####################
-def makeDF(base,inFile):
+def makeDF1(base,inFile):
     raw1=pd.read_csv(inFile)
 
     raw1=raw1.replace({'Staff...physically.injured': {'Yes': True, 'No': False, 'Unknown' : False},'Subject...injured.as.a.result.of.force.used': {'Yes': True, 'No': False, 'Unknown' : False}})
@@ -139,10 +146,6 @@ def makeDF(base,inFile):
 
     full=base.merge(val1DF, left_on='NAME', right_on='Borough', how='left')[['x','y','Borough','count']]
     full['Borough'].fillna('City of London', inplace=True)
-
-    full.at[13,"x"],full.at[13,"y"]=[val for val in full.at[13,"x"] if str(val)!='nan'],[val for val in full.at[13,"y"] if str(val)!='nan']
-    full.at[30,"x"],full.at[30,"y"]=[val for val in full.at[30,"x"] if str(val)!='nan'],[val for val in full.at[30,"y"] if str(val)!='nan']
-    full.at[31,"x"],full.at[31,"y"]=[val for val in full.at[31,"x"] if str(val)!='nan'],[val for val in full.at[31,"y"] if str(val)!='nan']
 
     #Get Data on what percent of indcidents tasers were used in
     raw1.Final_Effective_Tactic = raw1.Final_Effective_Tactic.str.replace(r'CED\s\(Taser\).*','Taser Involved')
@@ -162,6 +165,27 @@ def makeDF(base,inFile):
     full3=full2.merge(val3DF, on='Borough', how='left')[['x','y','Borough','count','Taser_Count','Taser_Per','Sub_Inj','Off_Inj']]
     full3['SubInj_Per'],full3['OffInj_Per']=(full3.Sub_Inj/full3['count'].std()*100).round(2),(full3.Off_Inj/full3['count'].std()*100).round(2)
     return(full3)
+    
+def makeDF2(base,inFile):
+    raw1=pd.read_csv(inFile)
+    
+    raw1=raw1.replace({'Staff...physically.injured': {'Yes': True, 'No': False, 'Unknown' : False},'Subject...injured.as.a.result.of.force.used': {'Yes': True, 'No': False, 'Unknown' : False}})
+    raw1['Borough'] = raw1['Borough'].replace(['City of Westminster'], 'Westminster')
+    raw1['Borough'].fillna('City of London', inplace=True)
+    
+    val2DF=pd.DataFrame({'Crime_Rate' : raw1.groupby('Borough')['Crime.rate...Index'].mean(),
+                     'Unemp_Rate' : raw1.groupby('Borough')['Unemployment.rate'].mean(),
+                     'Delib_Fires' : raw1.groupby('Borough')['Deliberate.Fires'].mean(),
+                     'Sub_WellBe' : raw1.groupby('Borough')['Subjective.well.being.average.score'].mean(),
+                     'Avg_GCSE' : raw1.groupby('Borough')['Average.Capped.GCSE.and.Equivalent.Point.Score.Per.Pupil'].mean(),
+                     'Pub_TransAcc' : raw1.groupby('Borough')['Public.Transport.Accessibility'].mean(),
+                     'HateCrime_Rate' : raw1.groupby('Borough')['HateCrime_per1000'].mean(),
+                     'Vote_Particip' : raw1.groupby('Borough')['Voting_Particip'].mean(),
+                     'GreenSpace' : raw1.groupby('Borough')['Homes.with.access.to.open.space...nature..and...greenspace'].mean()
+                    }).reset_index()
+    full=base.merge(val2DF, left_on="NAME", right_on='Borough', how='left')[['x','y','NAME','Crime_Rate','Unemp_Rate','Delib_Fires','Sub_WellBe','Avg_GCSE','Pub_TransAcc','HateCrime_Rate','Vote_Particip','GreenSpace']]
+    
+    return(full)
 
 ###################
 # PLOT WITH BOKEH #
@@ -237,6 +261,75 @@ def plot1(inDF):
 
     return(layout)
 
+def plot2(full):
+    colors=viridis(256)
+    col_Colors=[]
+
+    for datName in ['Crime_Rate','Unemp_Rate','Delib_Fires','Sub_WellBe','Avg_GCSE','Pub_TransAcc','HateCrime_Rate','Vote_Particip','GreenSpace']:
+        colList=[]
+        for borough in full['NAME']:
+            try:
+                val = full.loc[full['NAME'] == borough, datName].iloc[0]
+                if datName=='HateCrime_Rate':
+                    idx=int(val*55)
+                elif datName=='Vote_Particip':
+                    idx=int(val*200)
+                else:
+                    val = (val - full[datName].min()) * ((250-0)/(full[datName].max()-full[datName].min()))
+                    idx = int(val)
+                    colList.append(colors[idx])
+            except ValueError:
+                colList.append('grey')
+        col_Colors.append(colList)
+
+    
+    full['Crime Rate']=col_Colors[0]
+    crime_mapper = LinearColorMapper(palette=colors, low=full['Crime_Rate'].min(), high=full['Crime_Rate'].max())
+    full['Unemployment Rate']=col_Colors[1]
+    unemp_mapper = LinearColorMapper(palette=colors, low=full['Unemp_Rate'].min(), high=full['Unemp_Rate'].max())
+    full['Deliberate Fires Set']=col_Colors[2]
+    fires_mapper = LinearColorMapper(palette=colors, low=full['Delib_Fires'].min(), high=full['Delib_Fires'].max())
+    full['Subjective Well-Being Score']=col_Colors[3]
+    swb_mapper = LinearColorMapper(palette=colors, low=full['Sub_WellBe'].min(), high=full['Sub_WellBe'].max())
+    full['Average GCSE Score']=col_Colors[4]
+    gcse_mapper = LinearColorMapper(palette=colors, low=full['Avg_GCSE'].min(), high=full['Avg_GCSE'].max())
+    full['Public Transportation Accessibility']=col_Colors[5]
+    transp_mapper = LinearColorMapper(palette=colors, low=full['Pub_TransAcc'].min(), high=full['Pub_TransAcc'].max())
+    #~ full['Hate Crime Rate']=col_Colors[6]
+    #~ hate_mapper = LinearColorMapper(palette=colors, low=full['HateCrime_Rate'].min(), high=full['HateCrime_Rate'].max())
+    #~ full['Voter Participation Rate']=col_Colors[7]
+    #~ particip_mapper = LinearColorMapper(palette=colors, low=full['Vote_Particip'].min(), high=full['Vote_Particip'].max())
+    full['Homes with Access to Open Space and Nature']=col_Colors[8]
+    green_mapper = LinearColorMapper(palette=colors, low=full['GreenSpace'].min(), high=full['GreenSpace'].max())	
+	
+    full2=full.fillna(0)
+    sourceD=ColumnDataSource(full2)
+
+    #Create Base Plot
+    TOOLS = "pan,wheel_zoom,reset,hover,save"
+    p = figure(
+        title="Borough Descriptive Statistics", tools=TOOLS,
+        x_axis_location=None, y_axis_location=None)
+    p.grid.grid_line_color = None
+    p.hover.point_policy = "follow_mouse"
+    
+	#Add elements
+    r=p.patches('x', 'y', source=sourceD,
+              fill_color='Crime Rate',
+              fill_alpha=0.7, line_color="white", line_width=0.5)
+
+    cb_cselect = CustomJS(args=dict(r=r,sourceD=sourceD), code ="""
+        var selected_color = cb_obj.value;
+        r.glyph.fill_color.field = selected_color;
+        sourceD.change.emit()
+        """)
+    color_select = Select(title="Show:", value="Total Incidents", 
+           options=['Crime Rate','Unemployment Rate','Deliberate Fires Set','Subjective Well-Being Score','Average GCSE Score',
+           'Public Transportation Accessibility', 'Homes with Access to Open Space and Nature'],
+           callback=cb_cselect) #'Hate Crime Rate', 'Voter Participation Rate', 
+    layout = column(color_select, p)
+	
+    return(layout)
 
 ###############
 # WRAPPING UP #
